61be8042965e4a66acada546fa44666346036a2a
AzrShk <aceazr@gmail.com>
2019-05-10 10:22:05 +0530

    second comit

--- sw.js
+++ sw.js
@@ -1,25 +1,101 @@
-var cacheName = 'hello-pwa';
+var cacheName = 'pwa-test';
+
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+    './',
+    './index.html',
+	'./offline.html',
+	'./js/toast.js',
+	'./js/offline.js',
+	'./js/notification.js',
+	 './js/app.js',
+	 './images/push-off.png',
+    './images/push-on.png'
+    
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
+// Install Service Worker
+self.addEventListener('install', function(event) {
+
+    console.log('Service Worker: Installing....');
+
+    event.waitUntil(
+
+        // Open the Cache
+        caches.open(cacheName).then(function(cache) {
+            console.log('Service Worker: Caching App Shell at the moment......');
+
+            // Add Files to the Cache
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+
+// Fired when the Service Worker starts up
+self.addEventListener('activate', function(event) {
+
+    console.log('Service Worker: Activating....');
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(cacheNames.map(function(key) {
+                if( key !== cacheName) {
+                    console.log('Service Worker: Removing Old Cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', function(event) {
+
+    console.log('Service Worker: Fetch', event.request.url);
+
+    console.log("Url", event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request) ;
+        })
+    );
+});
+
+self.addEventListener('push', function(event) {
+
+		console.info('Event: Push');
+			var pushData;
+
+		  if (event.data) {
+			pushData = event.data.text();
+		  } else {
+			pushData = 'Push message no payload';
+		  }
+  var title = 'PWA blink';
+
+  var body = {
+    'body': 'Click to see the latest Offers',
+    'tag': 'pwa',
+    'icon': './images/48x48.png'
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, body)
   );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+
+self.addEventListener('notificationclick', function(event) {
+
+  var url = './latest.html';
+
+  event.notification.close(); //Close the notification
+
+  // Open the app and navigate to latest.html after clicking the notification
+  event.waitUntil(
+    clients.openWindow(url)
   );
+
 });
